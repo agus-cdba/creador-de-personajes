a0d1805c95841fbde08ed8375f1d2e05d747932c
Cordoba <agustincordoba754@gmail.com>
2025-07-01 22:20:13 -0300

    creador de personajes aleatorios.py

--- creador de personajes aleatorios.py
+++ creador de personajes aleatorios.py
@@ -1,27 +1,18 @@
 import random
 
-
-nombres = ["Frodo","Gandalf", "Thorin","Kirito","Kazuya","Asuna","Mikasa","Bulma","Konan","Kefla"]
-razas = ["Humano","Elfo","Enano","Demonio","Bestia","gyioyin", "dios", "Metahumano","Ciberhumano ","Hadas"]
-clases = ["Guerrero","Mago","Arquero","Curandero","Apoyo","Francotirador","Tanque","Herrero","Aldeano","Espadachin"]
-
-def generar_personaje(): 
-    nombre = random.choice(nombres)
-    raza = random.choice(razas)
-    clase = random.choice(clases)
-    vida = random.randint(50,101)
-    ataque = random.randint(10,51)
-    defensa = random.randint(5,31)
-
-    personaje ={
-        "nombre":nombre,
-        "raza":raza,
-        "clase":clase,
-        "vida":vida,
-        "ataque":ataque,
-        "defensa":defensa
+def generar_personaje():
+    nombres = ["Frodo","Gandalf", "Thorin","Kirito","Kazuya","Asuna","Mikasa","Bulma","Konan","Kefla"]
+    razas = ["Humano","Elfo","Enano","Demonio","Bestia","Gyioyin", "Dios", "Metahumano","Ciberhumano","Hada"]
+    clases = ["Guerrero","Mago","Arquero","Curandero","Apoyo","Francotirador","Tanque","Herrero","Aldeano","Espadachin"]
+    
+    return {
+        "nombre": random.choice(nombres),
+        "raza": random.choice(razas),
+        "clase": random.choice(clases),
+        "vida": random.randint(50, 100),
+        "ataque": random.randint(10, 50),
+        "defensa": random.randint(5, 30)
     }
-    return personaje 
 
-nuevo_personaje = generar_personaje()
-print(nuevo_personaje)
+pj = generar_personaje()
+print(pj)
